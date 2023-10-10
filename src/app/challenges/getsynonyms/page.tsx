d60b54559de5c26a43e50d93cab74f5b9180c47b
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-10-10 14:54:22 +0300

    synoyms are coming with request at 'getsynoyms'

--- src/app/challenges/getsynonyms/page.tsx
+++ src/app/challenges/getsynonyms/page.tsx
@@ -1,22 +1,51 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./getsynonyms.module.css";
+
+type TResponse = { word: string; score: number }[];
 
 const GetSynonyms = () => {
   let url = "https://api.datamuse.com/words?rel_syn=";
 
   const [word, setWord] = useState<string>("");
+  const [synonyms, setSynonyms] = useState<string[]>([]);
+  const [error, setError] = useState<boolean>(false);
+
+  const fetchWithWord = (word: string) => {
+    fetch(url + word)
+      .then((res) => res.json())
+      .then((resJson: TResponse) => {
+        setError(false);
+        if (resJson.length >= 4) {
+          setSynonyms(resJson.slice(0, 4).map((word) => word.word));
+        } else if (resJson.length > 0) {
+          setSynonyms(resJson.map((w) => w.word));
+        } else if (resJson.length == 0) {
+          setError(true);
+        }
+      });
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    fetch(url + word)
-      .then((res) => res.json())
-      .then((resJson) => console.log(resJson));
+    fetchWithWord(word);
   };
+
+  const lookToSynonymsofSynonym = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    wordFromSynonyms: string
+  ) => {
+    e.preventDefault();
+    setWord(wordFromSynonyms);
+  };
+
+  useEffect(() => {
+    fetchWithWord(word);
+  }, [word]);
 
   return (
     <main className={styles.getsynonymsContainer}>
-      <h1 style={{ fontSize: "3rem" }}>Not Finished</h1>
+      <h1 style={{ fontSize: "3rem", textAlign: "center" }}>Not Finished</h1>
       <form onSubmit={handleSubmit} className={styles.synonymsForm} action="">
         <input
           value={word}
@@ -25,14 +54,18 @@ const GetSynonyms = () => {
           placeholder="write a word"
         />
         <button>Check for synonyms</button>
+        {error && <div style={{ color: "red" }}>No synonyms found</div>}
       </form>
       <div className={styles.buttons}>
-        <button>asdasdasdasdX</button>
-        <button>asdasdasdasdX</button>
-        <button>asdasdasdasdX</button>
-        <button>asdasdasdasdX</button>
-        <button>asdasdasdasdX</button>
-        <button>Y</button>
+        {synonyms &&
+          synonyms.map((word) => (
+            <button
+              onClick={(e) => lookToSynonymsofSynonym(e, word)}
+              key={word}
+            >
+              {word}
+            </button>
+          ))}
       </div>
     </main>
   );
