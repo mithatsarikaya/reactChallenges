8eb90b2dc0558cfd8bf55ea6c850baad981812ea
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-10-11 19:58:27 +0300

    style added, putting number to the first array is succesfull

--- src/app/challenges/getinqueue/page.tsx
+++ src/app/challenges/getinqueue/page.tsx
@@ -1,7 +1,56 @@
+"use client";
+import { useState } from "react";
 import styles from "./getinqueue.module.css";
 
+type TQueueList = TQueue[];
+type TQueue = number[];
+
 const GetInQueue = () => {
-  return <main className={styles.container}>hey</main>;
+  const [newNumber, setNewNumber] = useState<number>(1);
+  const [qNumbers, setQNumbers] = useState<TQueueList>([[], [], [], [], []]);
+  console.log(qNumbers);
+  console.log(newNumber);
+
+  const handleSendToQueue = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!newNumber) return;
+    console.log("object");
+
+    let newNumbers = qNumbers.slice();
+    newNumbers[0].push(newNumber);
+
+    setQNumbers(newNumbers);
+  };
+
+  return (
+    <main className={styles.container}>
+      <form
+        onSubmit={(e) => handleSendToQueue(e)}
+        className={styles.inputForm}
+        action=""
+      >
+        <input
+          value={newNumber}
+          onChange={(e) => setNewNumber(Number(e.currentTarget.value))}
+          type="number"
+          name=""
+          id=""
+        />
+        <button>put to the queue</button>
+      </form>
+      <section className={styles.queueContainer}>
+        {qNumbers.map((q, idx) => (
+          <article key={idx} className={styles.queue}>
+            {q.map((a, idx) => (
+              <p key={idx} style={{ textAlign: "center" }}>
+                {a}
+              </p>
+            ))}
+          </article>
+        ))}
+      </section>
+    </main>
+  );
 };
 
 export default GetInQueue;
