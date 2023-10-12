a18abf68c35c0eb6fb91947432dc3ffe56d3705d
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-10-12 21:27:47 +0300

    board and its logic succesfull. game logic will be added

--- src/app/challenges/tictactoe/page.tsx
+++ src/app/challenges/tictactoe/page.tsx
@@ -5,6 +5,8 @@ const TicTacToe = () => {
   const createBlankRow = () => {
     return Array(3).fill("");
   };
+
+  const [isFinished, setIsFinished] = useState<boolean>(false);
   const [board, setBoard] = useState([
     createBlankRow(),
     createBlankRow(),
@@ -14,20 +16,48 @@ const TicTacToe = () => {
   const [mouseClicked, setMouseClicked] = useState(0);
   console.log(board);
 
+  const putXAndOAccordingly = (num: number) => {
+    return num % 2 == 0 ? "X" : "O";
+  };
+
+  console.log(putXAndOAccordingly(mouseClicked));
+
+  const handlePutLetter = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    rowIDUserClicked: number,
+    colIDUserClicked: number
+  ) => {
+    e.preventDefault();
+
+    setMouseClicked((prevNum) => prevNum + 1);
+
+    setBoard((prevBoard) =>
+      prevBoard.map((row, rowID) =>
+        rowID == rowIDUserClicked
+          ? row.map((col, colID) =>
+              colID == colIDUserClicked
+                ? putXAndOAccordingly(mouseClicked)
+                : col
+            )
+          : row
+      )
+    );
+  };
+
   return (
     <main className={styles.container}>
       <section className={styles.boxes}>
-        {board.map((row) =>
-          row.map((squ) => <button className={styles.box}></button>)
+        {board.map((row, rowIdx) =>
+          row.map((squ, colIdx) => (
+            <button
+              key={colIdx}
+              onClick={(e) => handlePutLetter(e, rowIdx, colIdx)}
+              className={styles.box}
+            >
+              {squ}
+            </button>
+          ))
         )}
-        {/* <button className={styles.box}></button>
-        <button className={styles.box}></button>
-        <button className={styles.box}></button>
-        <button className={styles.box}></button>
-        <button className={styles.box}></button>
-        <button className={styles.box}></button>
-        <button className={styles.box}></button>
-        <button className={styles.box}></button> */}
       </section>
     </main>
   );
