bd5e7914d781e47bd4eeaac9c7ba2afb8ab6c76c
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-10-12 19:47:59 +0300

    null board created

--- src/app/challenges/tictactoe/page.tsx
+++ src/app/challenges/tictactoe/page.tsx
@@ -1,6 +1,35 @@
+"use client";
+import { useState } from "react";
 import styles from "./tictactoe.module.css";
 const TicTacToe = () => {
-  return <main className={styles.container}>Tic Tac Toe</main>;
-};
+  const createBlankRow = () => {
+    return Array(3).fill("");
+  };
+  const [board, setBoard] = useState([
+    createBlankRow(),
+    createBlankRow(),
+    createBlankRow(),
+  ]);
 
+  const [mouseClicked, setMouseClicked] = useState(0);
+  console.log(board);
+
+  return (
+    <main className={styles.container}>
+      <section className={styles.boxes}>
+        {board.map((row) =>
+          row.map((squ) => <button className={styles.box}></button>)
+        )}
+        {/* <button className={styles.box}></button>
+        <button className={styles.box}></button>
+        <button className={styles.box}></button>
+        <button className={styles.box}></button>
+        <button className={styles.box}></button>
+        <button className={styles.box}></button>
+        <button className={styles.box}></button>
+        <button className={styles.box}></button> */}
+      </section>
+    </main>
+  );
+};
 export default TicTacToe;
