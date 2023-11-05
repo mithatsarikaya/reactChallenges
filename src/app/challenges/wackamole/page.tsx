9dfdd36610fae6075ed32d28d9999da13fe55794
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-11-05 22:26:19 +0300

    picking random numbers only from holes

--- src/app/challenges/wackamole/page.tsx
+++ src/app/challenges/wackamole/page.tsx
@@ -1,10 +1,51 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./wackamole.module.css";
 
+let moleUrl = "/wackamole/mole.png";
+let holeUrl = "/wackamole/hole.png";
+let initialState = Array(9)
+  .fill({})
+  .map((_, idx) => ({ id: idx, isHole: true }));
+
 const WackAMole = () => {
+  const [cells, setCells] = useState(initialState);
+
+  const showRandomMole = (numberToChange: number) => {
+    setCells((prevCells) =>
+      prevCells.map((cell) =>
+        cell.id == numberToChange ? { ...cell, isHole: !cell.isHole } : cell
+      )
+    );
+  };
+
+  const getRandomNumberFromOnlyHoles = () => {
+    let holeIDs = cells.filter((cell) => cell.isHole).map((cell) => cell.id);
+    return holeIDs[Math.floor(Math.random() * holeIDs.length)];
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      //   let randomNumber = Math.floor(Math.random() * cells.length);
+      showRandomMole(getRandomNumberFromOnlyHoles());
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [cells]);
+
+  console.log("checkrender");
+
   return (
     <main>
       <div className={styles.container}>
         <h1 className={styles.score}>Score 0</h1>
+        <article className={styles.photosContainer}>
+          {cells.map((cell) => (
+            <img key={cell.id} src={cell.isHole ? holeUrl : moleUrl} alt="" />
+          ))}
+        </article>
       </div>
     </main>
   );
