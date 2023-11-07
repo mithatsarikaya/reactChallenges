f1fac2cf293d078bf6186dc65ff48b5ef802d4eb
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-11-07 21:25:11 +0300

    board now has 8 unique numbers

--- src/app/challenges/memorynumbergame/page.tsx
+++ src/app/challenges/memorynumbergame/page.tsx
@@ -1,13 +1,25 @@
 import styles from "./memorynumbergame.module.css";
 
 const MemoryNumberGame = () => {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
   const BOARDSIZE = 16;
 
   let randomNumbersArray = [];
 
+  const isNumberAlreadyExistTwoTimes = (num: number, numArray: number[]) => {
+    return numArray.filter((n) => n == num).length == 2;
+  };
+
+  let getRandomNumberFromNumbersArray = () =>
+    numbers[Math.floor(Math.random() * numbers.length)];
+
   for (let num = 0; num < BOARDSIZE; num++) {
-    const element = numbers[Math.floor(Math.random() * numbers.length)];
+    let element = getRandomNumberFromNumbersArray();
+    //repick if number is exist 2 times already
+    while (isNumberAlreadyExistTwoTimes(element, randomNumbersArray)) {
+      element = getRandomNumberFromNumbersArray();
+    }
+
     randomNumbersArray.push(element);
   }
 
