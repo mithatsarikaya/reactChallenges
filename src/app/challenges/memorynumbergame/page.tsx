dfbfa5efd4ef4f566fbb0fe3684360d501426cde
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-11-07 20:48:08 +0300

    added some design and logic

--- src/app/challenges/memorynumbergame/page.tsx
+++ src/app/challenges/memorynumbergame/page.tsx
@@ -1,15 +1,27 @@
 import styles from "./memorynumbergame.module.css";
 
 const MemoryNumberGame = () => {
-  let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const BOARDSIZE = 16;
 
-  let showNumbers = Array(4)
-    .fill("_")
-    .map((_, idx) => numbers[idx]);
+  let randomNumbersArray = [];
+
+  for (let num = 0; num < BOARDSIZE; num++) {
+    const element = numbers[Math.floor(Math.random() * numbers.length)];
+    randomNumbersArray.push(element);
+  }
 
   return (
     <main>
-      <div className={styles.container}>{showNumbers}</div>
+      <div className={styles.container}>
+        <article className={styles.board}>
+          {randomNumbersArray.map((num, idx) => (
+            <button key={idx} className={styles.card}>
+              {num}
+            </button>
+          ))}
+        </article>
+      </div>
     </main>
   );
 };
