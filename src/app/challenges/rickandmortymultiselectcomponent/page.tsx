09be1fcf87fedf97c7f6d17a3493525f522741f6
mithatsarikaya <mithatsarikaya58@gmail.com>
2024-01-03 15:46:19 +0300

    working on checkbox

--- src/app/challenges/rickandmortymultiselectcomponent/page.tsx
+++ src/app/challenges/rickandmortymultiselectcomponent/page.tsx
@@ -1,26 +1,84 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./rickandmorty.module.css";
+import { ApiData } from "./types";
 
-const RickAndMortyChallenge = async () => {
-  let data = await fetch("https://rickandmortyapi.com/api/character").then(
-    (res) =>
-      res.json().then((resJson) => {
-        return resJson;
-      })
-  );
+const RickAndMortyChallenge = () => {
+  let getData = async (): Promise<ApiData> => {
+    let data = await fetch("https://rickandmortyapi.com/api/character").then(
+      (res) =>
+        res.json().then((resJson) => {
+          return resJson;
+        })
+    );
+    return data;
+  };
 
-  //   console.log(resJson);
-  console.log(data);
+  let getNamesImagesAndEpisodeNumbersWithIsSelected = async () => {
+    return (await getData()).results.map((result) => ({
+      id: result.id,
+      name: result.name,
+      imageUrl: result.image,
+      episodeCount: result.episode.length,
+      isSelected: false,
+    }));
+  };
 
+  const [characters, setCharacters] = useState<Awaited<
+    ReturnType<typeof getNamesImagesAndEpisodeNumbersWithIsSelected>
+  > | null>(null);
+
+  useEffect(() => {
+    getNamesImagesAndEpisodeNumbersWithIsSelected().then((res) =>
+      setCharacters(res)
+    );
+  }, []);
+
+  //TODO: working on checkbox
   return (
     <main className={styles.container}>
-      <div>
-        <select name="" id="" placeholder="write somet">
-          <option value="">1</option>
-          <option value="">2</option>
-          <option value="">3</option>
-          <option value="">4</option>
-        </select>
-      </div>
+      <article>
+        <div>
+          <div>
+            {characters?.map(
+              (character) => character.isSelected && <p>{character.name}</p>
+            )}
+            <input
+              className={styles.inputArea}
+              type="search"
+              name=""
+              id=""
+              placeholder="write smt"
+            />
+          </div>
+          <div>
+            <ul>
+              {characters &&
+                characters.map((character) => (
+                  <div className={styles.rowContainer} key={character.imageUrl}>
+                    <input
+                      type="checkbox"
+                      name=""
+                      id=""
+                      defaultChecked={character.isSelected}
+                    />
+                    <img
+                      className={styles.characterImage}
+                      src={character.imageUrl}
+                      alt=""
+                    />
+                    <div>
+                      <li className={styles.rowsInDropDown}>
+                        {character.name}
+                      </li>
+                      <span>{character.episodeCount} episodes</span>
+                    </div>
+                  </div>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </article>
     </main>
   );
 };
